b03e7470dae533af662c301a667e8fd6341e24f2
Pongsak Suvanpong <psksvp@gmail.com>
2023-08-17 14:05:40 +1000

    fixes from TempMon

--- Adafruit/src/board/PyPortal/PyPortal.h
+++ Adafruit/src/board/PyPortal/PyPortal.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#include "SPI.h"
-#include "Adafruit_GFX.h"
-#include "Adafruit_ILI9341.h"
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
 #include <Adafruit_SPIFlash.h>
-#include "Adafruit_ADT7410.h"
-#include "TouchScreen.h"
+#include <Adafruit_ADT7410.h>
+#include <TouchScreen.h>
 #include <SdFat.h>
 #include <WiFiNINA.h>
 
@@ -15,6 +15,8 @@ namespace PyPortal
   void enableBackLight(bool enable = true);
   void redLED(bool state);
   Adafruit_ILI9341& display();
-  TSPoint& touch();
+  bool touch();
+  TSPoint touchedCoord();
   uint16_t lightSensor();
+  float temperature();
 }
